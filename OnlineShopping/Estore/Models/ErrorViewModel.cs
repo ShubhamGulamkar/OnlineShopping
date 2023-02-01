0389decaddf753698aaba3960e96a49aa33b6b34
Shubham <shubhamkgulamkar@gmail.com>
2023-02-01 15:23:48 +0530

    Changes Added

--- OnlineShopping/Estore/Models/ErrorViewModel.cs
+++ OnlineShopping/Estore/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace Estore.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+	<h2>HELLO FROM GIT<h2>
+	<P>AND WELCOME TO GIT</p>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
